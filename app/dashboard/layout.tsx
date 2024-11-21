d69961eef08fd75efd6bb771679580c004473ebc
陈堂会 <chenth09@vanke.com>
2024-11-21 17:20:28 +0800

    🔒 publish basic verison

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,25 @@
+import { ReactNode } from "react"
+import DashboardSideBar from "./_components/dashboard-side-bar"
+import DashboardTopNav from "./_components/dashbord-top-nav"
+import { isAuthorized } from "@/utils/data/user/isAuthorized"
+import { redirect } from "next/dist/server/api-utils"
+import { currentUser } from "@clerk/nextjs/server"
+
+export default async function DashboardLayout({ children }: { children: ReactNode }) {
+
+  const user = await currentUser()
+  const { authorized, message } = await isAuthorized(user?.id!)
+  if (!authorized) {
+    console.log('authorized check fired')
+  }
+  return (
+    <div className="grid min-h-screen w-full lg:grid-cols-[280px_1fr]">
+      <DashboardSideBar />
+      <DashboardTopNav >
+        <main className="flex flex-col gap-4 p-4 lg:gap-6">
+          {children}
+        </main>
+      </DashboardTopNav>
+    </div>
+  )
+}
