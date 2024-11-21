5948a4687674c32491be22de0d1e64fc7024f6f4
陈堂会 <chenth09@vanke.com>
2024-11-21 17:15:57 +0800

    🔒 init basic project template & config

---
+++ next.config.ts
@@ -0,0 +1,33 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [{
+      protocol: 'https',
+      hostname: 'images.unsplash.com',
+      port: '',
+      pathname: '/**'
+    }, {
+      protocol: 'https',
+      hostname: 'seo-heist.s3.amazonaws.com',
+      port: '',
+      pathname: '/**'
+    }, {
+      protocol: 'https',
+      hostname: 'github.com',
+      port: '',
+      pathname: '/**'
+    }, {
+      protocol: 'https',
+      hostname: 'ansubkhan.com',
+      port: '',
+      pathname: '/**'
+    }, {
+      protocol: 'https',
+      hostname: 'utfs.io',
+      port: '',
+      pathname: '/**'
+    }]
+  }
+};
+export default nextConfig;
